9c22544503d6e9541ae77244784b79c95c3416eb
Kurenairisu <minec323@gmail.com>
2025-09-24 17:09:06 +0800

    init

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Small script to enable 'More Info' toggle on touch devices
-document.addEventListener('click', function(e){
-  const btn = e.target.closest('.more-btn');
-  if(btn){
-    const card = btn.closest('.movie-card');
-    if(card) card.classList.toggle('show');
-  }
+document.addEventListener("click", function (e) {
+	const btn = e.target.closest(".more-btn");
+	if (btn) {
+		const card = btn.closest(".movie-card");
+		if (card) card.classList.toggle("show");
+	}
 });
